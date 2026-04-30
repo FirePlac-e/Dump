8485ffe41b0b9cf67a34e4cc4197901876d984dd
FirePlac-e <harry1.roquier@epitech.eu>
2026-04-30 17:34:20 +0200

    [+] adding first version of the DUMP

---
+++ INSTALL.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+clear
+apt upgrade
+
+#installing vscode
+apt update
+apt install -y software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt update
+apt install -y code
+
+#installing chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+#installing discord
+snap install discord
+
+#installing EPITECH_DUMP
+echo "INSTALLING TOOLS AND PACKAGES FOR EPITECH'S DUMP"
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+add-apt-repository -y -s ppa:epitech/ppa
+add-apt-repository -y -s universe
+apt update
+echo "postfix postfix/mailname string epitech.eu" | debconf-set-selections
+echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
+export DEBIAN_FRONTEND=noninteractive
+apt install -y wget
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/llvm.asc
+echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/llvm.asc] https://apt.llvm.org/noble/ llvm-toolchain-noble-20 main" | tee /etc/apt/sources.list.d/llvm.list
+apt update
+apt install -y clang-20
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-20 100 && update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-20 100
+apt install -y epitech-cpool
+apt install -y epitech-emacs
+apt install -y epitech-vim
+apt purge -y postfix
+snap install teams-for-linux
+apt install -y git
+apt install -y docker.io
+apt update
+
+#installing Hypland
+git clone -b 25.10 --depth=1 https://github.com/JaKooLit/Ubuntu-Hyprland.git ~/Ubuntu-Hyprland-25.10
+cd ~/Ubuntu-Hyprland-25.10
+chmod +x install.sh
+./install.sh
